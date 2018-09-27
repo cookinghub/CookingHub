6f69d047e45b5e38c1fd11911e057308be598bf1
Yaelda <yaelka@gmail.com>
2018-09-27 14:53:27 +0300

    Dockerfile push and tag with git sha1

--- scripts/deploy-to-ecr.sh
+++ scripts/deploy-to-ecr.sh
@@ -1,7 +1,11 @@
-#! /bin/bash
+#! /bin/bash -x
+
+DOCKER_IMAGE_TAG=$1
 
 # run docker login
 $(aws ecr get-login --no-include-email --region us-east-1)
 
 # use maven to deploy docker image
 mvn dockerfile:push
+mvn dockerfile:tag@tag-version -D dockerTag=${DOCKER_IMAGE_TAG}
+mvn dockerfile:push@tag-version -D dockerTag=${DOCKER_IMAGE_TAG}
